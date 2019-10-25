e5208812a035eebc341a98e65b7f9f6537b5d38b
Abhishek Varde <34768409+abhishekvarde@users.noreply.github.com>
2019-10-25 21:53:06 +0530

    Update settings.py

--- SellMyBook/settings.py
+++ SellMyBook/settings.py
@@ -136,5 +136,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'miniprojectsdl@gmail.com'
-EMAIL_HOST_PASSWORD = 'Myminiproject'
+EMAIL_HOST_PASSWORD = '************'
 
